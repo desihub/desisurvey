4b0acfd80fa9f88119287960fb8382c03de606a7
dpearson1983 <dpearson1983@gmail.com>
2015-07-21 10:06:58 -0500

    Brought variable names into compliance with coding guidelines, adjusted some lines lengths, and removed some commented lines of code that are no longer necessary.

--- py/desisurvey/nextfield.py
+++ py/desisurvey/nextfield.py
@@ -85,46 +85,48 @@ def get_next_field(dateobs, skylevel, seeing, transparency, previoustiles,
         with some calib programname?  Or?
     """
     
-    """ Below is an algorithm for calculating the local apparent sidereal time. 
-    astropy should be able to this (I think), but I keep getting an error when I try 
-    to use those functions (and I'm pretty sure I'm importing all the necessary 
-    components of astropy). A bunch of lines have been commented out which were 
-    used for testing purposes. """
+    """ 
+        Below is an algorithm for calculating the local apparent sidereal time. 
+        astropy should be able to this (I think), but I keep getting an error when I try 
+        to use those functions (and I'm pretty sure I'm importing all the necessary 
+        components of astropy). A bunch of lines have been commented out which were 
+        used for testing purposes. 
+    """
     
     #Find the Julian date of the previous midnight
     if (dateobs-math.floor(dateobs) >= 0.5):
-        JD_0 = math.floor(dateobs)+0.5
+        jd_0 = math.floor(dateobs)+0.5
     elif (dateobs-math.floor(dateobs) < 0.5):
-        JD_0 = math.floor(dateobs)-0.5
+        jd_0 = math.floor(dateobs)-0.5
         
-    D_0 = JD_0-2451545.0 #Difference between last Julian midnight and J2000
-    D = dateobs-2451545.0 #Difference between observation data and J2000
-    T = D_0/36525 #Fraction of Julian century that's past since J2000
+    d_0 = jd_0-2451545.0 #Difference between last Julian midnight and J2000
+    d = dateobs-2451545.0 #Difference between observation date and J2000
+    t = d_0/36525 #Fraction of Julian century that's past since J2000
     
     #Calculate the sideral time in Greenwich for the last Julian midnight
-    GMST_0 = 100.4606184+36000.77005361*T+0.00038793*T**2-2.6E-08*T**3
+    gmst_0 = 100.4606184+36000.77005361*t+0.00038793*t**2-2.6E-08*t**3
     
     #Add correction for the number of hours that have past since midnight
-    GMST = GMST_0+0.25068447733746215*(dateobs-JD_0)*24*60
+    gmst = gmst_0+0.25068447733746215*(dateobs-jd_0)*24*60
     
     #Calculate the equation of equinoxes
-    L = 280.47+0.98565*D
-    Omega = 125.04-0.052954*D
-    delPsi = -0.000319*math.sin(Omega*math.pi/180)-0.00024*math.sin(2*L*math.pi/180)
-    E = 23.4393-0.0000004*D
+    l = 280.47+0.98565*d
+    omega = 125.04-0.052954*d
+    del_psi = -0.000319*math.sin(omega*math.pi/180)-0.00024*math.sin(2*l*math.pi/180)
+    e = 23.4393-0.0000004*d
     
-    eqeq = delPsi*math.cos(E*math.pi/180)
+    eqeq = del_psi*math.cos(e*math.pi/180)
     
     #Correct with the equation of equinoxes to get the current apparent sidereal time
-    GAST = GMST+eqeq
+    gast = gmst+eqeq
     
     #Add the longitude of the observatory to get the local sidereal time
-    LAST = GAST-111.5984796
+    last = gast-111.5984796
         
     #Shift the local sidereal time into the range of 0 to 360 degrees
-    if LAST >= 360:
-        n = math.floor(LAST/360)
-        LAST = LAST-360*n
+    if last >= 360:
+        n = math.floor(last/360)
+        last = last-360*n
         
     #hour = math.floor(LAST/15)
     #minute = math.floor((LAST/15-hour)*60)
@@ -144,35 +146,42 @@ def get_next_field(dateobs, skylevel, seeing, transparency, previoustiles,
     #Use PyAstronomy to calculate the position of the Sun.
     pos_sun = pyasl.sunpos(dateobs,full_output=True)
     
-    #print("RA_sun = " + str(float(pos_sun[1])) + " Dec_sun = " + str(float(pos_sun[2])))
+    #print("ra_sun = " + str(float(pos_sun[1])) + " dec_sun = " + str(float(pos_sun[2])))
     
     #Check to see if the Sun is up.
-    RA_sun = float(pos_sun[1])
-    Dec_sun = float(pos_sun[2])
-    HA_sun = LAST - RA_sun
-    if (HA_sun < 0):
-        HA_sun = HA_sun+360
-    if (HA_sun > 360):
-        HA_sun = HA_sun-360
-    #print("HA_sun = " + str(HA_sun))
-    ALT_sun = (math.asin(math.sin(Dec_sun*math.pi/180)*math.sin(31.9614929*math.pi/180)+math.cos(Dec_sun*math.pi/180)*math.cos(31.9614929*math.pi/180)*math.cos(HA_sun*math.pi/180)))*(180/math.pi)
-    #print("ALT_sun = " + str(ALT_sun))
+    ra_sun = float(pos_sun[1])
+    dec_sun = float(pos_sun[2])
+    ha_sun = last - ra_sun
+    if (ha_sun < 0):
+        ha_sun = ha_sun+360
+    if (ha_sun > 360):
+        ha_sun = ha_sun-360
+    #print("ha_sun = " + str(ha_sun))
+    
+    #Calculate the altitude of the Sun to determine if it is up
+    alt_sun = (math.asin(math.sin(dec_sun*math.pi/180)
+                         *math.sin(31.9614929*math.pi/180)
+                         +math.cos(dec_sun*math.pi/180)
+                         *math.cos(31.9614929*math.pi/180)
+                         *math.cos(ha_sun*math.pi/180)))*(180/math.pi)
+    
+    #print("alt_sun = " + str(alt_sun))
     
     #Print warning if the Sun is up. We may decide this should do more than just warn
-    if (ALT_sun >= 0):
+    if (alt_sun >= 0):
         print("WARNING: The sun is currently up!")
         
     #Find the position of the Moon using PyAstronomy
     pos_moon = pyasl.moonpos(dateobs)
     
-    RA_moon = float(pos_moon[0])
-    Dec_moon = float(pos_moon[1])
+    ra_moon = float(pos_moon[0])
+    dec_moon = float(pos_moon[1])
     
-    print("RA_moon = " + str(RA_moon) + " Dec_moon = " + str(Dec_moon))
+    print("ra_moon = " + str(ra_moon) + " dec_moon = " + str(dec_moon))
     
     #Find the phase of the moon using PyAstronomy
-    moonPhase = pyasl.moonphase(dateobs)
-    phase = float(moonPhase)
+    moon_phase = pyasl.moonphase(dateobs)
+    phase = float(moon_phase)
     
     print(phase)
     
@@ -181,7 +190,7 @@ def get_next_field(dateobs, skylevel, seeing, transparency, previoustiles,
     tiles_file = open("desi-tiles-full.par.txt","r")
     
     #Declare some variables to store data from the file
-    Tiles = []
+    tiles = []
     idnum = []
     ra = []
     dec = []
@@ -198,8 +207,8 @@ def get_next_field(dateobs, skylevel, seeing, transparency, previoustiles,
     nextfield = 0
     #Read the data from the file and find the next field
     for line in tiles_file:
-        Tiles.append(line)
-        c = Tiles[j].split(" ")
+        tiles.append(line)
+        c = tiles[j].split(" ")
         if (c[0] == "STRUCT1"):
             idnum.append(int(c[1]))
             ra.append(float(c[2]))
@@ -209,34 +218,24 @@ def get_next_field(dateobs, skylevel, seeing, transparency, previoustiles,
             ebv_med.append(float(c[6]))
             airmass.append(float(c[7]))
             exposefac.append(float(c[8]))
-            HA = LAST-ra[i]
-            if HA < 0:
-                HA = HA + 360
-            if HA > 360:
-                HA = HA - 360
-            ALT = (math.asin(math.sin(dec[i]*math.pi/180)*math.sin(31.9614929*math.pi/180)+math.cos(dec[i]*math.pi/180)*math.cos(31.9614929*math.pi/180)*math.cos(HA*math.pi/180)))*(180/math.pi)
-            if (ALT >=0):
-                #possibletiles = possibletiles + 1
-                #uptiles_file.write(str(idnum[i]) + " " + str(ra[i]) + " " + str(dec[i]) + " " + str(HA) + " " + str(ALT) + "\n")
-                if (dec[i] < mindec and ra[i] >= LAST-1 and ra[i] <= LAST-1):
+            ha = last-ra[i]
+            if ha < 0:
+                ha = ha + 360
+            if ha > 360:
+                ha = ha - 360
+            alt = (math.asin(math.sin(dec[i]*math.pi/180)
+                             *math.sin(31.9614929*math.pi/180)
+                             +math.cos(dec[i]*math.pi/180)
+                             *math.cos(31.9614929*math.pi/180)
+                             *math.cos(ha*math.pi/180)))*(180/math.pi)
+            if (alt >= 0):
+                if (dec[i] < mindec and ra[i] >= last-1 and ra[i] <= last+1):
                     mindec = dec[i]
                     nextfield = idnum[i]
-            #else:
-                #impossibletiles = impossibletiles + 1
-                #downtiles_file.write(str(idnum[i]) + " " + str(ra[i]) + " " + #str(dec[i]) + " " + str(HA) + " " + str(ALT) + "\n")
             i = i + 1
         j = j + 1
     
-    tiles_file.close()
-    
-    #print("Total number of tiles in the file: " + str(i))
-    
-    #print("The number of tiles that are possible to observe is: " + str(possibletiles))
-    #print("The number of tiles that are not possible to observer is: " + str(impossibletiles))
-    
-    #print("The next field that should be observed is: " + str(nextfield) + "\n")
-    #print(str(idnum[nextfield-1]) + " " + str(ra[nextfield-1]) + " " + str(dec[nextfield-1]) + " " + str(passnum[nextfield-1]) + " " + str(in_desi[nextfield-1]) + " " + str(ebv_med[nextfield-1]) + " " + str(airmass[nextfield-1]) + " " + str(exposefac[nextfield-1]))
-    
+    tiles_file.close()    
     
     #raise NotImplementedError
     
@@ -250,13 +249,13 @@ def get_next_field(dateobs, skylevel, seeing, transparency, previoustiles,
 inputting a Julian date of observation. They also calculate the execution time for
 purposes of optimizing."""
 
-#dateobs = float(raw_input('Enter the date of observation: '))
-#skylevel = 0
-#transparency = 0
-#previoustiles = []
-#programname = 'DESI'
-#start_time = time.time()
-#next_field = get_next_field(dateobs, skylevel, transparency, previoustiles, programname)
+dateobs = float(raw_input('Enter the date of observation: '))
+skylevel = 0
+transparency = 0
+previoustiles = []
+programname = 'DESI'
+start_time = time.time()
+next_field = get_next_field(dateobs, skylevel, transparency, previoustiles, programname)
 
-#print("Total execution time: %s seconds" % (time.time()-start_time))
-#print next_field
+print("Total execution time: %s seconds" % (time.time()-start_time))
+print next_field
