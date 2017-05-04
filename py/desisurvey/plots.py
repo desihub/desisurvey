a64bfcd4eefc9c0eb51d6b298a68e7996c22625f
David Kirkby <dkirkby@uci.edu>
2017-05-03 18:31:46 -0700

    Update plot_observed() to use the new Progress tracker

--- py/desisurvey/plots.py
+++ py/desisurvey/plots.py
@@ -101,15 +101,21 @@ def plot_sky_passes(ra, dec, passnum, z, clip_lo=None, clip_hi=None,
     return fig, ax
 
 
-def plot_observations(start_date=None, stop_date=None, what='EXPTIME',
-                      filename='obslist_all.fits', verbose=False, save=None):
+def plot_observed(progress, include='observed', start_date=None, stop_date=None,
+                  what='exptime', print_summary=False, save=None):
     """Plot a summary of observed tiles.
 
-    Reads the file ``obsall_list.fits`` and uses :func:`plot_sky_passes` to
-    display a summary of observed tiles in each pass.
+    Reports progress tracked by :class:`desisurvey.progress.Progress.` using
+    :func:`plot_sky_passes` to display a summary of observed tiles in each pass.
 
     Parameters
     ----------
+    progress : desisurvey.progress.Progress
+        Progress tracker to use.
+    include : 'all', 'observed', or 'completed'
+        Specify which tiles to include in the summary. The 'observed'
+        selection will include tiles that have been observed at least
+        once but have not yet reached their SNR**2 goal.
     start_date : date or None
         Plot observations starting on the night of this date, or starting
         with the first observation if None. Must be convertible to a
@@ -120,14 +126,9 @@ def plot_observations(start_date=None, stop_date=None, what='EXPTIME',
         :func:`desisurvey.utils.get_date`.
     what : string
         What quantity to plot for each planned tile. Must be a
-        column name in the obsall_list FITS file.  Useful values include
-        EXPTIME, OBSSN2, AIRMASS, SEEING.
-    filename : string
-        Name of the output file listing all the observations. Unless an
-        absolute path is provided, the filename is relative to the
-        :meth:`configuration output path
-        <desisurvey.config.Configuration.set_output_path>`.
-    verbose : bool
+        column name in the summary table returned by
+        :meth:`desisurvey.progress.Progress.get_summary`.
+    print_summary : bool
         Print a summary of observed tiles.
     save : string or None
         Name of file where plot should be saved.
@@ -137,43 +138,38 @@ def plot_observations(start_date=None, stop_date=None, what='EXPTIME',
     tuple
         Tuple (figure, axes) returned by ``plt.subplots()``.
     """
-    config = desisurvey.config.Configuration()
-    t = astropy.table.Table.read(config.get_path(filename))
-    if what not in t.colnames:
-        raise ValueError('Valid names are: {0}'
-                         .format(','.join(t.colnames)))
-
-    start_date = desisurvey.utils.get_date(start_date or t['MJD'][0])
-    stop_date = desisurvey.utils.get_date(stop_date or (t['MJD'][-1] + 1.0))
-    if start_date >= stop_date:
-        raise ValueError('Expected start_date < stop_date.')
+    if start_date:
+        start_date = desisurvey.utils.get_date(start_date)
+        mjd_min = desisurvey.local_noon_on_date(start_date).mjd
+    else:
+        start_date = desisurvey.utils.get_date(progress.first_mjd)
+        mjd_min = None
+    if stop_date:
+        stop_date = desisurvey.utils.get_date(stop_date)
+        mjd_max = desisurvey.local_noon_on_date(stop_date).mjd
+    else:
+        stop_date = desisurvey.utils.get_date(progress.last_mjd)
+        mjd_max = None
     date_label = '{0} to {1}'.format(start_date, stop_date)
 
-    # Convert date range to MJDs at local noon.
-    start_mjd = desisurvey.utils.local_noon_on_date(start_date).mjd
-    stop_mjd = desisurvey.utils.local_noon_on_date(stop_date).mjd
+    if mjd_min or mjd_max:
+        # Copy progress within these MJD limits.
+        progress = progress.copy_range(mjd_min, mjd_max)
 
-    # Trim table to requested observations.
-    sel = (t['STATUS'] > 0) & (t['MJD'] >= start_mjd) & (t['MJD'] < stop_mjd)
-    t = t[sel]
+    # Create the summary table to use.
+    t = progress.get_summary(include)
+    if what not in t.colnames:
+        raise ValueError('Valid names are: {0}'.format(','.join(t.colnames)))
 
-    if verbose:
+    if print_summary:
         print('Observing summary for {0}:'.format(date_label))
         for pass_num in range(8):
-            sel_pass = t['PASS'] == pass_num
-            n_exps = np.count_nonzero(sel_pass)
-            n_tiles = len(np.unique(t['TILEID'][sel_pass]))
-            print('Observed {0:7d} tiles with {1:5d} repeats from PASS {2}'
-                  .format(n_tiles, n_exps - n_tiles, pass_num))
-        n_exps = len(t)
-        n_tiles = len(np.unique(t['TILEID']))
-        print('Observed {0:7d} tiles with {1:5d} repeats total.'
-              .format(n_tiles, n_exps - n_tiles))
+            print('Completed {0:5.1f} tiles for pass {1}.'
+                  .format(progress.completed(only_passes=pass_num), pass_num))
 
     label = '{0} ({1})'.format(what, date_label)
     return desisurvey.plots.plot_sky_passes(
-        t['RA'], t['DEC'], t['PASS'], t[what],
-        label=label, save=save)
+        t['ra'], t['dec'], t['pass'], t[what], label=label, save=save)
 
 
 def plot_program(ephem, start_date=None, stop_date=None, style='localtime',
@@ -604,7 +600,7 @@ def plot_planner(p, where=None, when=None, night_summary='dark',
         Tuple (figure, axes) returned by ``plt.subplots()``.
     """
     import matplotlib.pyplot as plt
-    
+
     if where and when:
         raise ValueError('Cannot specify both where and when.')
 
