c6c49441e94ff9b583eabce43e85557d240c92e8
David Kirkby <dkirkby@uci.edu>
2017-05-10 14:53:31 -0700

    Make backwards compatible with astropy < 1.2 (overwrite -> clobber)

--- py/desisurvey/utils.py
+++ py/desisurvey/utils.py
@@ -156,7 +156,10 @@ def update_iers(save_name='iers_frozen.ecsv', num_avg=1000):
 
     # Save the table. The IERS-B table provided with astropy uses the
     # ascii.cds format but astropy cannot write this format.
-    iers.write(save_name, format='ascii.ecsv', overwrite=True)
+    try:
+        iers.write(save_name, format='ascii.ecsv', overwrite=True)
+    except TypeError:
+        iers.write(save_name, format='ascii.ecsv', clobber=True)
     print('Wrote updated table to {0}.'.format(save_name))
 
 
