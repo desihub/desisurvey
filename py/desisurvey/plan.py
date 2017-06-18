8e2fafc742b78bc9123a0757d910508a617ac9af
David Kirkby <dkirkby@uci.edu>
2017-06-18 10:04:58 -0700

    Implement planning rules parser

--- py/desisurvey/plan.py
+++ py/desisurvey/plan.py
@@ -3,6 +3,7 @@
 from __future__ import print_function, division
 
 import os
+import re
 
 import yaml
 
@@ -22,8 +23,11 @@ import desisurvey.progress
 import desisurvey.schedule
 
 
-def get_groups(file_name='plan.yaml'):
-    """Read group definitions from the specified YAML file.
+def load_plan(file_name='plan.yaml'):
+    """Load an observing plan from the specified file.
+
+    Read tile group definitions and observing rules from the specified
+    YAML file.
     """
     # Load the table of tiles in the DESI footprint.
     tiles = astropy.table.Table(
@@ -34,6 +38,9 @@ def get_groups(file_name='plan.yaml'):
     NGC = (tiles['RA'] > 75.0) & (tiles['RA'] < 300.0)
     SGC = ~NGC
 
+    # Initialize regexp for parsing "GROUP_NAME(PASS)"
+    parser = re.compile('([^\(]+)\(([0-7])\)$')
+
     # Get the full path of the YAML file to read.
     if os.path.isabs(file_name):
         full_path = file_name
@@ -42,15 +49,19 @@ def get_groups(file_name='plan.yaml'):
         full_path = astropy.utils.data._find_pkg_data_path(
             os.path.join('data', file_name))
 
-    # Read the YAML file.
+    # Read the YAML file into memory.
     with open(full_path) as f:
         config = yaml.safe_load(f)
 
     group_names = []
     group_ids = np.zeros(num_tiles, int)
-    for group_name in config['groups']:
+    group_rules = {}
+
+    for group_name in config:
         group_sel = np.ones(num_tiles, bool)
-        node = config['groups'][group_name]
+        node = config[group_name]
+
+        # Parse optional geographical attribute.
         cap = node.get('cap')
         if cap == 'N':
             group_sel[~SGC] = False
@@ -62,29 +73,65 @@ def get_groups(file_name='plan.yaml'):
         dec_max = node.get('dec_max')
         if dec_max is not None:
             group_sel[dec >= float(dec_max)] = False
+
+        # Parse required "passes" attribute.
         passes = node.get('passes')
         if passes is None:
             raise RuntimeError(
                 'Missing required passes for {0}.'.format(group_name))
         passes = [int(p) for p in str(passes).split(',')]
+
+        # Create GROUP(PASS) combinations.
         for p in passes:
-            group_names.append('{0}[{1:d}]'.format(group_name, p))
+            pass_name = '{0}({1:d})'.format(group_name, p)
+            group_names.append(pass_name)
             group_id = len(group_names)
             pass_sel = group_sel & (passnum == p)
             if np.any(group_ids[pass_sel] != 0):
                 other_id = np.unique(group_ids[pass_sel])[-1]
                 raise RuntimeError(
                     'Some tiles assigned to multiple groups: {0}, {1}.'
-                    .format(group_names[other_id - 1], group_names[-1]))
+                    .format(group_names[other_id - 1], pass_name))
             group_ids[pass_sel] = group_id
+            group_rules[pass_name] = {}
             print(group_id, group_names[-1], np.count_nonzero(pass_sel))
+
+        # Parse rules for this group.
+        rules = node.get('rules')
+        if rules is None:
+            raise RuntimeError(
+                'Missing required rules for {0}.'.format(group_name))
+        for target in rules:
+            target_parsed = parser.match(target)
+            if not target_parsed or target_parsed.groups(1) == group_name:
+                raise RuntimeError('Invalid rule target: {0}'.format(target))
+            for trigger in rules[target]:
+                if trigger == 'START':
+                    continue
+                trigger_parsed = parser.match(trigger)
+                if not trigger_parsed:
+                    raise RuntimeError(
+                        'Invalid rule trigger: {0}.'.format(trigger))
+                try:
+                    new_weight = float(rules[target][trigger])
+                except ValueError:
+                    raise RuntimeError(
+                        'Invalid new weight for trigger {0}: {1}.'
+                        .format(trigger, rules[target][trigger]))
+            group_rules[target][trigger] = new_weight
+
+    # Check that all groups have at least one rule.
+    for pass_name in group_rules:
+        if group_rules[pass_name] == {}:
+            raise RuntimeError('Missing rules for {0}.'.format(pass_name))
+
     # Check that all tiles are assigned to exactly one group.
     if np.any(group_ids == 0):
         orphans = (group_ids == 0)
         passes = ','.join([str(s) for s in np.unique(passnum[orphans])])
         raise RuntimeError('{0} tiles in passes {1} not assigned to any group.'
                            .format(np.count_nonzero(orphans), passes))
-    return group_names, group_ids
+    return group_names, group_ids, group_rules
 
 
 def baseline(tiles):
@@ -245,4 +292,5 @@ def update_required(plan, progress):
 
 
 if __name__ == '__main__':
-    get_groups()
+    """Regression test for loading a plan from a YAML file"""
+    load_plan()
