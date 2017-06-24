9da5e80ff59667c848ef1bd135a13b4f7f4a5ba8
David Kirkby <dkirkby@uci.edu>
2017-06-24 16:32:04 -0700

    Add missing import

--- py/desisurvey/scripts/surveyinit.py
+++ py/desisurvey/scripts/surveyinit.py
@@ -4,12 +4,16 @@ This is normally run once, at the start of the survey, and saves its results
 to a FITS file surveyinit.fits.  With the default parameters, the running time
 is about 1 hour.
 
+This script will create the ephemerides and scheduler files if necessary,
+which takes about 15 additional minutes.
+
 To run this script from the command line, use the ``surveyinit`` entry point
 that is created when this package is installed, and should be in your shell
 command search path.
 """
 from __future__ import print_function, division, absolute_import
 
+import os
 import argparse
 import cProfile
 
