e3a201ce28b61629ea562b59f39bd63db22229c4
David Kirkby <dkirkby@uci.edu>
2017-08-12 16:09:15 -0700

    Flesh out movie script

--- py/desisurvey/scripts/surveymovie.py
+++ py/desisurvey/scripts/surveymovie.py
@@ -8,6 +8,17 @@ from __future__ import print_function, division, absolute_import
 
 import argparse
 
+import numpy as np
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec
+import matplotlib.animation
+
+import astropy.time
+import astropy.units as u
+
 import desiutil.log
 
 import desisurvey.ephemerides
@@ -28,6 +39,12 @@ def parse(options=None):
     parser.add_argument('--progress', default='progress.fits', metavar='FITS',
         help='name of FITS file with progress record')
     parser.add_argument(
+        '--start', type=str, default=None, metavar='DATE',
+        help='movie starts on the evening of this day, formatted as YYYY-MM-DD')
+    parser.add_argument(
+        '--stop', type=str, default=None, metavar='DATE',
+        help='movie stops on the morning of this day, formatted as YYYY-MM-DD')
+    parser.add_argument(
         '--output-path', default=None, metavar='PATH',
         help='output path where output files should be written')
 
@@ -36,7 +53,33 @@ def parse(options=None):
     else:
         args = parser.parse_args(options)
 
+    # Validate start/stop date args and covert to datetime objects.
+    # Unspecified values are taken from our config.
+    config = desisurvey.config.Configuration()
+    if args.start is None:
+        args.start = config.first_day()
+    else:
+        try:
+            args.start = desisurvey.utils.get_date(args.start)
+        except ValueError as e:
+            raise ValueError('Invalid start: {0}'.format(e))
+    if args.stop is None:
+        args.stop = config.last_day()
+    else:
+        try:
+            args.stop = desisurvey.utils.get_date(args.stop)
+        except ValueError as e:
+            raise ValueError('Invalid stop: {0}'.format(e))
+    if args.start >= args.stop:
+        raise ValueError('Expected start < stop.')
+
     return args
+
+
+def wrap(angle, offset=-60):
+    """Wrap values in the range [0, 360] to [offset, offset+360].
+    """
+    return np.fmod(angle - offset + 360, 360) + offset
 
 
 def main(args):
@@ -64,3 +107,71 @@ def main(args):
 
     # Load progress.
     progress = desisurvey.progress.Progress(args.progress)
+    tiles = progress._table
+    ra = wrap(tiles['ra'])
+    dec = tiles['dec']
+    print(np.min(ra), np.max(ra))
+    print(np.min(dec), np.max(dec))
+
+    # Get a list of exposures in [start, stop].
+    exposures = progress.get_exposures(
+        args.start, args.stop, tile_fields='tileid,index,ra,dec,pass',
+        exp_fields='mjd,exptime,snr2cum')
+    tile_index = exposures['index']
+    snr2 = exposures['snr2cum']
+    num_exp = len(tile_index)
+    log.info('Found {0} exposures from {1} to {2}.'
+             .format(num_exp, args.start, args.stop))
+
+    # Calculate each exposure's LST window.
+    exp_midpt = astropy.time.Time(
+        exposures['mjd'] + exposures['exptime'] / 86400., format='mjd',
+        location=desisurvey.utils.get_location())
+    lst_midpt = exp_midpt.sidereal_time('apparent').to(u.deg).value
+    lst_len = exposures['exptime'] / 240. # convert from seconds to degrees.
+    lst = np.empty((num_exp, 2))
+    lst[:, 0] = wrap(lst_midpt - 0.5 * lst_len)
+    lst[:, 1] = wrap(lst_midpt + 0.5 * lst_len)
+
+    # power of 2 so that inches are exact.
+    dpi = 32.
+    # Use 1080p for compatibility with youtube.
+    width, height = 1920, 1080
+
+    # Initialize figure and axes.
+    figure = plt.figure(
+        frameon=False,figsize=(width / dpi, height / dpi), dpi=dpi)
+    grid = matplotlib.gridspec.GridSpec(3, 3)
+    grid.update(left=0, right=1, bottom=0, top=0.97, hspace=0, wspace=0)
+    axes = []
+    scatters = []
+    passnum = 0
+    for row in range(3):
+        for col in range(3):
+            # Create the axes for this pass.
+            ax = plt.subplot(grid[row, col])
+            ax.set_xticks([])
+            ax.set_yticks([])
+            axes.append(ax)
+            # Top-right corner is reserved for a plot.
+            if row == 0 and col == 2:
+                ax.set_xlim(0, 1)
+                ax.set_ylim(0, 1)
+                plt.plot([0, 1], [0, 1], 'k:')
+                continue
+            ax.set_xlim(-55, 293)
+            ax.set_ylim(-20, 77)
+            # Draw the tile outlines for this pass.
+            sel = (tiles['pass'] == passnum)
+            ntiles = np.count_nonzero(sel)
+            fc = np.zeros((ntiles, 4))
+            ec = np.zeros((ntiles, 4))
+            ec[:, 3] = 0.25
+            s = np.full(ntiles, 85.)
+            scatters.append(ax.scatter(
+                ra[sel], dec[sel], s=s, facecolors=fc, edgecolors=ec, lw=1))
+
+            passnum += 1
+
+
+    plt.savefig('movie.png')
