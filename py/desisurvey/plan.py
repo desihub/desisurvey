4a85b4d1df755cde5810f60139362f6c825217f2
David Kirkby <dkirkby@uci.edu>
2018-11-01 17:08:10 -0700

    Implement afternoon planning class

--- py/desisurvey/plan.py
+++ py/desisurvey/plan.py
@@ -180,3 +180,99 @@ def update_available(plan, progress, night, ephem, fa_delay, fa_delay_type):
                 # Record the night number when these tiles were first covered.
                 plan['available'][under] |= new_avail
     return plan
+
+
+class Planner(object):
+    """Coordinate afternoon planning activities.
+    """
+    def __init__(self, fiberassign_cadence='monthly', tiles_file=None):
+        if fiberassign_cadence not in ('daily', 'monthly'):
+            raise ValueError('Invalid fiberassign_cadence: "{}".'.format(fiberassign_cadence))
+        self.fiberassign_cadence = fiberassign_cadence
+        config = desisurvey.config.Configuration()
+        self.tiles = desisurvey.tiles.get_tiles(tiles_file)
+        self.ephem = desisurvey.ephemerides.Ephemerides()
+        # Set all priorities to one.
+        self.tile_priority = np.ones(self.tiles.ntiles, float)
+        # Any passes with no fiber-assignment dependency are initially available.
+        self.tile_covered = np.full(self.tiles.ntiles, -1)
+        self.tile_countdown = np.full(self.tiles.ntiles, 1)
+        self.tile_available = np.zeros(self.tiles.ntiles, bool)
+        self.fiberassign_order = config.fiber_assignment_order
+        self.tile_diameter = 2 * config.tile_radius().to(u.deg).value
+        for passnum in self.tiles.passes:
+            if 'P{}'.format(passnum) not in self.fiberassign_order.keys:
+                # Mark tiles in this pass as initially available.
+                sel = self.tiles.passnum == passnum
+                self.tile_covered[sel] = 0
+                self.tile_available[sel] = True
+                print('Pass {} available for initial observing.'.format(passnum))
+
+    def initialize(self, night):
+        self.initial_night = night
+        self.last_fiberassign = None
+        return self.tile_available, self.tile_priority
+
+    def fiberassign(self, night, completed):
+        day_number = (night - self.initial_night).days
+        print('Running fiber assignment on {} (day number {}) with {} tiles completed.'
+              .format(night, day_number, np.count_nonzero(completed)))
+        for passnum in self.tiles.passes:
+            under = self.tiles.passnum == passnum
+            over = np.zeros_like(under)
+            key = 'P{}'.format(passnum)
+            if key not in self.fiberassign_order.keys:
+                if not np.all(self.tile_available[under]):
+                    raise RuntimeError('Expected all tiles available in pass {}.'
+                                       .format(passnum))
+            else:
+                # Build a mask of all tiles covering this pass.
+                overpasses = getattr(self.fiberassign_order, key)()
+                for overpass in overpasses.split('+'):
+                    if not len(overpass) == 2 and overpass[0] == 'P':
+                        raise RuntimeError(
+                            'Invalid pass in fiber_assignment_order: {}.'
+                            .format(overpass))
+                    over |= (self.tiles.passnum == int(overpass[1]))
+                ##print('pass {} covered by {} tiles'.format(passnum, np.count_nonzero(over)))
+                # Calculate the overlap matrix between tiles in this pass (under) and
+                # all covering passes (over).
+                overlapping = desisurvey.utils.separation_matrix(
+                    self.tiles.tileRA[under], self.tiles.tileDEC[under],
+                    self.tiles.tileRA[over], self.tiles.tileDEC[over], self.tile_diameter)
+                # Identify all tiles in this pass whose covering tiles are completed.
+                covered = np.all(~overlapping | completed[over], axis=1)
+                # Which tiles have been newly covered since the last call to fiberassign?
+                new_covered = covered & (self.tile_covered[under] == -1)
+                ##print('pass {} has {} tiles covered ({} new)'
+                ##      .format(passnum, np.count_nonzero(covered), np.count_nonzero(new_covered)))
+                if np.any(new_covered):
+                    new_tiles = self.tiles.tileID[under][new_covered]
+                    ##print('{} new tiles available in pass {}: {}.'
+                    ##      .format(len(new_tiles), passnum, ','.join([str(ID) for ID in new_tiles])))
+                    # Record the day number when these tiles were first covered.
+                    new = under.copy()
+                    new[under] = new_covered
+                    self.tile_covered[new] = day_number
+        # Identify tiles that have been covered but not yet had fiber assignment run.
+        ready = ~self.tile_available & (self.tile_covered >= 0)
+        # Run fiber assignment on ready tiles that have completed their countdown.
+        run_now = ready & (self.tile_countdown == 0)
+        self.tile_available[run_now] = True
+        # Update delay countdown for the remaining ready tiles.
+        delayed = ready & (self.tile_countdown > 0)
+        self.tile_countdown[delayed] -= 1
+        print('fiber assigned {} tiles, with {} delayed.'
+              .format(np.count_nonzero(run_now), np.count_nonzero(delayed)))
+
+    def afternoon_plan(self, night, complete):
+        # Update fiber assignments.
+        if self.fiberassign_cadence == 'monthly':
+            dt = self.ephem.get_night(night)['nearest_full_moon']
+            run_fiberassign = (dt > -0.5) and (dt <= 0.5)
+        else:
+            run_fiberassign = True
+        if run_fiberassign:
+            self.fiberassign(night, complete)
+
+        return self.tile_available, self.tile_priority
