3c51700c30b0f518f20652f895fcd9e4a99feefd
michaelJwilson <mjwilson@lbl.gov>
2019-08-16 10:05:24 -0700

    Added .gitignore structure

--- py/desisurvey/plot-ecliptic.py
+++ py/desisurvey/plot-ecliptic.py
@@ -1,4 +1,4 @@
-import  matplotlib           as      mpl ##; mpl.use('PDF')
+import  matplotlib           as      mpl
 
 import  desisurvey
 import  astropy.io.fits      as      fits
@@ -19,15 +19,7 @@ from    astropy.coordinates  import  SkyCoord, EarthLocation, AltAz
 from    desitarget.geomask   import  circles
 
 ##
-program          =  'Dark'
-
-##
-##  run              = '-op'
-##  nnights          =    26
-
-run              = '-y5'
-nnights          = 1642
-
+program          =  'Gray'
 program2int      = {'Dark': 0, 'Gray': 1, 'Bright': 2}
 
 tiles            = Table(fits.open('/global/cscratch1/sd/mjwilson/svdc2019c2/survey/basetiles/original/schlafly-tiles.fits')[1].data)
@@ -39,6 +31,7 @@ tiles            = tiles[tiles['RA'].quantity <  290.]  ##  280.
 tiles            = tiles[tiles['DEC'].quantity > -10.]  ##   -5.                                                                                                                                             
 tiles            = tiles[tiles['DEC'].quantity <  80.]  ##   75.                                                                                                                                          
 '''
+'''
 tiles.sort('CENTERID')
 
 cmap   = plt.get_cmap('viridis')
@@ -48,34 +41,33 @@ norm   = mpl.colors.Normalize()
 
 pl.clf()
 
-hrs_visible        = np.loadtxt('visibility{}/visibility-nofullmoon-{}-{}.txt'.format(run, nnights, program2int[program]))
+nnights            = 1642
+hrs_visible        = np.loadtxt('visibility/visibility-nofullmoon-{}-{}.txt'.format(nnights, program2int[program]))
 
 normed_visibility  = np.sum(hrs_visible, axis=0) / nnights
-
-isin               = normed_visibility > 0.0
 
 ##  Wrap ra. 
 ##  tiles['RA']       -= 90.
 ##  tiles['RA'][tiles['RA'] < 0.0] += 360.
 
-##  Plot ecliptic                                                                                                                                                                                              
-moons = np.loadtxt('visibility{}/moons.txt'.format(run))
-plt.plot(moons[:,0], moons[:,1], c='k', marker='.', lw=0, rasterized=True, markersize=1)
-
-circles(tiles['RA'].quantity[isin], tiles['DEC'].quantity[isin], s=1.67, lw=1., ec='k', c=normed_visibility[isin], alpha=0.4, cmap=cmap, norm=norm)
+circles(tiles['RA'].quantity, tiles['DEC'].quantity, s=1.67, lw=1., ec='k', c=normed_visibility, alpha=0.4, cmap=cmap, norm=norm)
+'''
+##  Plot ecliptic
+moons = np.loadtxt('moons.txt')
+pl.plot(moons[:,0], moons[:,1], 'ko', marker=2)
+##  circles(moons[:,0], moons[:,1], s=50., fc=None, ec='k')
 
 pl.xlim(10., 360.)
 pl.ylim(-20.,  80.)
 
 pl.title(program)
  
-plt.colorbar(label='Mean hrs / per night')
-
 pl.xlabel(r'Right ascension [deg.]')
 pl.ylabel(r'Declination [deg.]')
 
 plt.tight_layout()
 plt.gca().invert_xaxis()
 
-##  pl.show()
-pl.savefig('plots/visibility{}-{}-{}.png'.format(run, nnights, program2int[program]))                                                                                                        
+pl.show()
+  
+##  pl.savefig('plots/visibility-nofullmoon-{}-{}.pdf'.format(nnights, program2int[program]))                                                                                                                                                                                                
