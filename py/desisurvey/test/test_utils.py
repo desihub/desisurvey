d2bbbb86a31a765390cd7b70396b8333f48b2e59
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2026-08-10 13:36:39 -0700

    Test the HA limit parser and interpolator

--- py/desisurvey/test/test_utils.py
+++ py/desisurvey/test/test_utils.py
@@ -227,3 +227,48 @@ class TestUtils(Tester):
         assert np.allclose(utils.separation_matrix([90], [0], [90], [90]), 90.)
         assert np.allclose(utils.separation_matrix([0], [0], [0], [-45]), 45.)
         assert np.allclose(utils.separation_matrix([330], [0], [30], [0]), 60.)
+
+    def test_parse_ha_limit_spec(self):
+        dec, ha = utils.parse_ha_limit_spec('-30:0.5, 0:3.0, 30:4.0')
+        assert np.allclose(dec, [-30, 0, 30])
+        # Limits are converted from hours to degrees.
+        assert np.allclose(ha, [7.5, 45., 60.])
+        # Nodes need not be given in order.
+        dec2, ha2 = utils.parse_ha_limit_spec('30:4.0, -30:0.5, 0:3.0')
+        assert np.allclose(dec, dec2)
+        assert np.allclose(ha, ha2)
+        # Extra whitespace and a trailing separator are tolerated.
+        dec3, ha3 = utils.parse_ha_limit_spec('  -30 : 0.5 ,0:3.0,30:4.0,  ')
+        assert np.allclose(dec, dec3)
+        assert np.allclose(ha, ha3)
+        # A malformed specification must raise rather than silently disable
+        # the limit.
+        with self.assertRaises(ValueError):
+            utils.parse_ha_limit_spec('')
+        with self.assertRaises(ValueError):
+            utils.parse_ha_limit_spec('-30:0.5')
+        with self.assertRaises(ValueError):
+            utils.parse_ha_limit_spec('-30:0.5, junk')
+        with self.assertRaises(ValueError):
+            utils.parse_ha_limit_spec('-30:0.5, 0:x')
+        with self.assertRaises(ValueError):
+            utils.parse_ha_limit_spec('-30:0.5, 0:3.0:9')
+
+    def test_max_ha_by_dec(self):
+        spec = '-30:0.5, -20:1.5, 0:3.0, 40:4.0, 60:5.0'
+        # Node values are reproduced exactly.
+        assert np.allclose(utils.max_ha_by_dec([-30, -20, 0, 40, 60], spec),
+                           [7.5, 22.5, 45., 60., 75.])
+        # Interpolation is linear in declination, on the limit in hours.
+        assert np.allclose(utils.max_ha_by_dec(-25, spec), 15.)
+        assert np.allclose(utils.max_ha_by_dec(-10, spec), 33.75)
+        # Outside the tabulated range the end values are held.
+        assert np.allclose(utils.max_ha_by_dec([-90, -40], spec), 7.5)
+        assert np.allclose(utils.max_ha_by_dec([70, 90], spec), 75.)
+        # A ceiling can only tighten the limit, never loosen it.
+        assert np.allclose(utils.max_ha_by_dec(60, spec, ceiling=75.), 75.)
+        assert np.allclose(utils.max_ha_by_dec(60, spec, ceiling=50.), 50.)
+        assert np.allclose(utils.max_ha_by_dec(-30, spec, ceiling=75.), 7.5)
+        # Shape is preserved.
+        dec = np.array([[-30., 0.], [40., 60.]])
+        assert utils.max_ha_by_dec(dec, spec).shape == dec.shape
